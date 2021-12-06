9cbe9b2725dd365a583a1120dec01aa5d4e112c5
Darshit Sarna <op.dare7@gmail.com>
2021-12-06 23:50:43 +0530

    circle active update

--- 2. Progress Steps/script.js
+++ 2. Progress Steps/script.js
@@ -1,4 +1,32 @@
 const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const circle = document.querySelectorAll('.circle');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1 // first circle active
+
+next.addEventListener('click', () => {
+    currentActive++
+    if (currentActive > circles.length) { //so current active doesn't go beyond the specified length
+        currentActive = circles.length
+    }
+    update() // update circles
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+    update()
+})
+
+const update = () => {
+    circles.forEach((circle, id) => {
+        if (id < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circles.classList.remove('active')
+        }
+    })
+}
